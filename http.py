de07a40b0f8be7682f06dee41564ad87579681c3
karl.wang <104814962@qq.com>
2017-04-21 22:49:24 +0800

    添加buffer

--- http.py
+++ http.py
@@ -8,7 +8,12 @@ s.bind((host, port))
 while True:
     s.listen(5)
     connection, address = s.accept()
-    request = connection.recv(1024)
+    buffer_size = 1000
+    r = b''
+    while True:
+        request = connection.recv(buffer_size)
+        if len(request) < buffer_size:
+            break
     response = b'HTTP/1.1 200 very ok\r\n\r\n<h1>hello world<h1>'
     connection.sendall(response)
     connection.close()
