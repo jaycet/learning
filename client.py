c36bf02f90a242b91ceb50fe3069e1001fde2c66
karl.wang <104814962@qq.com>
2017-04-21 22:04:50 +0800

    提交client

---
+++ client.py
@@ -0,0 +1,17 @@
+import socket
+
+
+s = socket.socket()
+#https连接如下：
+#import ssl
+#s = ssl.wrap_socket(socket.socket())
+host = 'www.harmontronics.com'
+port = 80
+s.connect((host, port))
+ip, port = s.getsockname()
+print('本机的ip：{}，port：{}'.format(ip, port))
+http_request = 'GET / HTTP/1.1\r\nhost:{}\r\nConnection: close\r\n\r\n'.format(host, port)
+request = http_request.encode('utf-8')
+s.send(request)
+response = s.recv(1023)
+print(response.decode('utf-8'))
