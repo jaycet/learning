e75256c90d1ecbc4d4d3dc71791cf579a94c011b
karl.wang <104814962@qq.com>
2017-04-21 22:54:55 +0800

    修改port

--- client.py
+++ client.py
@@ -11,7 +11,7 @@ port = 80
 s.connect((host, port))
 ip, port = s.getsockname()
 print('本机的ip：{}，port：{}'.format(ip, port))
-http_request = 'GET / HTTP/1.1\r\nhost:{}\r\nConnection: close\r\n\r\n'.format(host, port)
+http_request = 'GET / HTTP/1.1\r\nhost: {}:{}\r\nConnection: close\r\n\r\n'.format(host, port)
 request = http_request.encode('utf-8')
 s.send(request)
 response = s.recv(1023)
